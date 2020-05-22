ed03a30ae6a807e78bc2b4fd908df45601da6690
riyandharne-123 <rockydods123@gmail.com>
2020-05-23 03:32:03 +0530

    Updates

---
+++ precache-manifest.a47930f80f52970d9ccd91a45b5a3f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accc7836d9262e8cd783e26bdd7af358",
+    "url": "/RestApiReactApp/index.html"
+  },
+  {
+    "revision": "8e522c80c89482909023",
+    "url": "/RestApiReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38332446fa01d8159103",
+    "url": "/RestApiReactApp/static/js/2.e1e4f405.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RestApiReactApp/static/js/2.e1e4f405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e522c80c89482909023",
+    "url": "/RestApiReactApp/static/js/main.07e130dd.chunk.js"
+  },
+  {
+    "revision": "4c30c2eb749bfaddd5ea",
+    "url": "/RestApiReactApp/static/js/runtime-main.b7184af2.js"
+  }
+]);
